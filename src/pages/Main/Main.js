624b764299e3f4d59d079e599f417754646649eb
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-10-12 14:32:47 +0200

    Add initial implementation

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
 function Main() {
-  return <div>Main works!!!</div>;
+  const list = [1, 10, 2, 6, 5, 3];
+
+  const highestProduct = array => {
+    const isListValid = array.every(
+      item => typeof item === 'number' && Number.isInteger(item)
+    );
+
+    if (isListValid) {
+      const sortedList = [...array].sort((a, b) => b - a);
+      const highestThreeNumbers = sortedList.slice(0, 3);
+
+      return highestThreeNumbers.reduce((acc, next) => acc * next, 1);
+    }
+
+    return 'Incorrect list of numbers';
+  };
+
+  return <div>{highestProduct(list)}</div>;
 }
 
 export default Main;
